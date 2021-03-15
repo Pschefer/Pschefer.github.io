5d09fc18e1f0d8987a81317f5af86ddc5ec09ae6
Philip Schefer <pschefer@student.ethz.ch>
2021-03-15 14:44:54 +0100

    changed sidebar

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -3,6 +3,7 @@ ticTacToeGame.start();
 
 function TicTacToeGame() {
   const board = new Board();
+  const board_copy = new Board();
   const humanPlayer = new HumanPlayer(board);
   const computerPlayer = new ComputerPlayer(board);
   let turn = 0;
@@ -67,12 +68,56 @@ function Board() {
   };
 }
 
-function ComputerPlayer(board) {
+function ComputerPlayer(board, board_copy) {
+  // Implement Minimax algorithm here to make the computer smarter
   this.takeTurn = function () {
     let availablePositions = board.positions.filter((p) => p.innerText === "");
-    const move = Math.floor(Math.random() * (availablePositions.length - 0));
-    availablePositions[move].innerText = "O";
+    let availPos_copy = board_copy.positions.filter((p) => p.innerText === "");
+
+    let bestScore = -Infinity;
+    let bestMove;
+    let count = 0;
+
+    for (let i = 0; i < availablePositions.length; i++) {
+      availPos_copy[i].innerText = "O";
+      let score = MiniMax(board_copy, 0, true, availablePositions.length);
+
+      if (score > bestScore) {
+        bestScore = score;
+
+        bestMove = i;
+      }
+    }
+
+    //const move = Math.floor(Math.random() * (availablePositions.length - 0));
+    availablePositions[bestMove].innerText = "O";
   };
+}
+
+let scores = {
+  X: 1,
+  O: -1,
+  tie: 0,
+};
+
+function MiniMax(board, depth, isMaximizing, available_pos) {
+  let result = board.checkForWinner();
+
+  if (result) {
+    let score = scores[result];
+    return score;
+  }
+
+  if (isMaximizing) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < available_pos; i++) {
+      let score = MiniMax(board, depth + 1, true);
+      bestScore = Math.max(score, bestScore);
+    }
+    console.log(bestScore);
+
+    return bestScore;
+  }
 }
 
 function HumanPlayer(board) {
